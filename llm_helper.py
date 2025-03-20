2bbd62c9724beca0bb32efa705f72c5561894959
Muskan Ara <muskan17ara@gmail.com>
2025-03-20 13:56:27 +0530

    updated code

--- llm_helper.py
+++ llm_helper.py
@@ -6,7 +6,8 @@ load_dotenv()
 
 groq_api_key = os.getenv("GROQ_API_KEY")
 
-llm = ChatGroq(groq_api_key= groq_api_key, model_name="llama-3.2-90b-text-preview")
+#llm = ChatGroq(groq_api_key= groq_api_key, model_name="llama3:latest")
+llm = ChatGroq(groq_api_key=groq_api_key, model_name="llama3-8b-8192")
 
 
 if __name__ == "__main__":
